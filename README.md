fcf12e7db889430da904a4f439634fa4ab11b279
2dog <wugy315@126.com>
2018-01-17 21:10:01 +0800

    新增删除Excel指定sheet指定行(支持多行删除)相关代码

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
 # wolfgy-util
 Util collection of mine.我的个人工具类集合。
 
+## Java相关工具类
+#### [Excel相关工具类](https://github.com/wolgy/wolfgy-util/blob/master/src/main/java/com/wolfgy/util/java/ExcelUtil.java)
+1. deleteSpecifiedRows(String inputFilePath,String outputFilePath,List<Integer> rowList,int sheetIndex) throws Exception  
+作用:删除excel文件中指定行  
+示例:传入参数`inputFilePath="C:\\test.xlsx"`，`outputFilePath="C:\\test-handled.xlsx"`，`rowList={2,3}`，`sheetIndex=0`。则本方法将会读取路径为`C:\\test.xlsx`的Excel文件，删除第1个sheet页的第2行和第3行，并将处理好的新Excel输出至`C:\\test-handled.xlsx`。  
+参考:[利用poi开源jar包操作Excel时删除行内容与直接删除行的区别](http://blog.csdn.net/b_h_l/article/details/8255247)  
 ## Android相关工具类
 #### [ResourceUtil/R文件相关工具类](https://github.com/wolgy/wolfgy-util/blob/master/src/main/java/com/wolfgy/util/android/ResourceUtil.java)
 1. getResourceFieldName(Class<?> r , String resourceType , int value)  
-作用：获取R文件中资源值对应的字段名。  
-示例：`drawable`文件夹下名为`ic_catelog_0006010`的资源文件，
+作用:获取R文件中资源值对应的字段名。  
+示例:`drawable`文件夹下名为`ic_catelog_0006010`的资源文件，
 在`R`文件中生成的资源字段为`public static final int ic_catelog_0006010=0x7f070061`。
 调用本方法传入`resourceType="drawable"`,`value=0x7f070061`,方法返回`"ic_catelog_0006010"`。
 2. getResourceFieldValue(Class<?> r , String resourceType , String name)  
-作用：获取R文件中资源字段名对应的值。  
-示例：`drawable`文件夹下名为`ic_catelog_0006010`的资源文件，
+作用:获取R文件中资源字段名对应的值。  
+示例:`drawable`文件夹下名为`ic_catelog_0006010`的资源文件，
 在`R`文件中生成的资源字段为`public static final int ic_catelog_0006010=0x7f070061`。
 调用本方法传入`resourceType="drawable"`,`name="ic_catelog_0006010"`,方法返回`0x7f070061`。
-####  [GridViewUtil/GridView相关工具类](https://github.com/wolgy/wolfgy-util/blob/master/src/main/java/com/wolfgy/util/android/GridViewUtil.java)
+
+#### [GridViewUtil/GridView相关工具类](https://github.com/wolgy/wolfgy-util/blob/master/src/main/java/com/wolfgy/util/android/GridViewUtil.java)
 1. setGridViewHeightBasedOnChildren(GridView gridView)  
-作用：设置GridView高度(在调用setAdapter()方法后调用此方法)。可用于解决GridView与ListView/RecyclerView等可滑动视图嵌套时只显示一行的问题。  
+作用:设置GridView高度(在调用setAdapter()方法后调用此方法)。可用于解决GridView与ListView/RecyclerView等可滑动视图嵌套时只显示一行的问题。  
 整理自:[利用反射获取GridView的高度](http://blog.csdn.net/jys1115/article/details/46045211)
---
+++ src/main/java/com/wolfgy/util/java/ExcelUtil.java
@@ -0,0 +1,72 @@
+package com.wolfgy.util.java;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtil {
+
+	/**
+	 * 
+	 * <p>
+	 * Description:删除excel指定行<br />
+	 * </p>
+	 * @author wolfgy
+	 * @version 0.1 2018年1月17日
+	 * @param inputFilePath 输入文件路径(eg:"C://test.xlsx")
+	 * @param outputFilePath 输出文件路径(最终输出文件类型会与输入文件类型保持一致)
+	 * @param rowList 要删除的具体行数
+	 * @param sheetIndex 该excel文件中所要处理的sheet索引(第一个sheet的index为0)
+	 * @throws Exception
+	 * void
+	 */
+	public static void deleteSpecifiedRows(String inputFilePath,String outputFilePath,List<Integer> rowList,int sheetIndex) throws Exception{
+		//获取文件名后缀判断文件类型
+        String fileType = inputFilePath.substring(inputFilePath.lastIndexOf(".") + 1,
+                inputFilePath.length());
+        //根据文件类型及文件输入流新建工作簿对象
+        FileInputStream is = new FileInputStream(inputFilePath); 
+        Workbook workbook = null;
+        if (fileType.equals("xls")) {
+        	workbook = new HSSFWorkbook(is);            
+        } else if (fileType.equals("xlsx")) {
+        	workbook = new XSSFWorkbook(is);
+        } else {
+        	is.close(); 
+            throw new Exception("您输入的excel格式不正确");
+        }
+        Sheet sheet = workbook.getSheetAt(sheetIndex); 
+        int lastRowNum = sheet.getLastRowNum();
+        //倒序排列行数集合
+        Collections.sort(rowList,new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				if (o1 > o2) {
+					return -1;
+				}else if(o1 < o2){
+					return 1;
+				}
+				return 0;
+			}			
+		});
+        //遍历删除数据行
+        for (int row : rowList) {
+            sheet.shiftRows(row,lastRowNum--,-1);
+		}
+        //保证输入输出的文件类型一致。
+        FileOutputStream os = new FileOutputStream(outputFilePath.substring(0,
+        		inputFilePath.lastIndexOf(".") + 1)+fileType);  
+        //写文件
+        workbook.write(os);  
+        is.close();  
+        os.close();  
+        workbook.close();
+	}
+}
