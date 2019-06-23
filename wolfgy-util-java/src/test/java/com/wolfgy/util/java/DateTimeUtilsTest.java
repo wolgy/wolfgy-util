4b24c49181ecf185284227681c20b2f6a7d83428
wolfgy <wugy315@126.com>
2019-06-24 02:24:59 +0800

    调整项目结构

--- wolfgy-util-java/src/test/java/com/wolfgy/util/java/DateTimeUtilsTest.java
+++ wolfgy-util-java/src/test/java/com/wolfgy/util/java/DateTimeUtilsTest.java
@@ -6,7 +6,7 @@ public class DateTimeUtilsTest {
 
 	@Test
 	public void test() {
-		long days = DateTimeUtils.getWorkingDays("2019-06-01", "2019-06-23");
+		long days = DateTimeUtils.getWorkingDays("2018-11-15", "2018-12-17");
 		System.out.println("Working days:"+days);
 	}
 }
